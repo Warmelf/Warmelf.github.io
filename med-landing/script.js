a93d14c07100d3bf57f8b5b44442e397e37ffb23
Warmelf <warmelf17@gmail.com>
2022-07-06 19:00:36 +0300

    add med-landing

---
+++ med-landing/script.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.scrollto').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+let menuToggle = document.querySelector('#menu__toggle');
+let main = document.querySelector('main');
+
+main.addEventListener('click', function() {
+    menuToggle.checked = false;
+})
